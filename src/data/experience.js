0835c639530383b118db9b617290e0fd3e4f7d15
Milad Mehri <memm.milad@gmail.com>
2025-03-08 19:47:47 -0800

    Update experience.js

--- src/data/experience.js
+++ src/data/experience.js
@@ -1,18 +1,5 @@
 const experience = [
   {
-    company: "Multifaceted Innovation in NeuroTechnology",
-    position: "Software Engineer",
-    date: "Jan 2025 - Present",
-    tech: "React Native, Accessibility, Bluetooth Integration",
-    bulletPoints: [
-      "Developing a React Native mobile application for neurotechnology research.",
-      "Ensuring app accessibility by integrating Switch Control and VoiceOver support.",
-      "Connecting and calibrating EEG headsets for accurate data collection.",
-    ],
-    link: "https://ubcmint.github.io/",
-    image: `${process.env.PUBLIC_URL}/mint_logo.png`,
-  },
-  {
     company: "Visual Cognition Lab",
     position: "Software Developer",
     date: "Feb 2025 - Present",
@@ -24,6 +11,19 @@ const experience = [
     ],
     link: "https://viscoglab.psych.ubc.ca/",
     image: `${process.env.PUBLIC_URL}/vcl_logo.png`,
+  },
+  {
+    company: "Multifaceted Innovation in NeuroTechnology",
+    position: "Software Engineer",
+    date: "Jan 2025 - Present",
+    tech: "React Native, Accessibility, Bluetooth Integration",
+    bulletPoints: [
+      "Developing a React Native mobile application for neurotechnology research.",
+      "Ensuring app accessibility by integrating Switch Control and VoiceOver support.",
+      "Connecting and calibrating EEG headsets for accurate data collection.",
+    ],
+    link: "https://ubcmint.github.io/",
+    image: `${process.env.PUBLIC_URL}/mint_logo.png`,
   },
 ];
 
